b40bca6b37b63236e2e0c544073563b2b25cb354
1ogon1 <konovalenkoruslan@gmail.com>
2024-08-31 14:10:20 +0300

    fix get timeout

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,13 +7,13 @@ export class AppController {
   private readonly timeout: number;
 
   constructor(config: ConfigService) {
-    this.timeout = config.get('TIMEOUT');
+    this.timeout = config.get('PING_TIMEOUT');
   }
 
   @Get(':ip')
   async getHello(@Param('ip') ip: string): Promise<boolean> {
     try {
-      console.log(`--> Pinging ${ip} with a timeout of ${this.timeout}ms`);
+      console.log(`--> Pinging ${ip} with a timeout of ${this.timeout} s`);
 
       const probe = await ping.promise.probe(ip, {
         timeout: this.timeout,
